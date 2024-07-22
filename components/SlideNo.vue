da151bae1331e9a789ea7afc7fb3547ad12cabc5
johannchopin <31794680+johannchopin@users.noreply.github.com>
2024-07-22 17:31:40 +0200

    Initial commit

---
+++ components/SlideNo.vue
@@ -0,0 +1,12 @@
+<script>
+import {  useNav } from '@slidev/client'
+
+const { currentSlideNo } = useNav()
+
+</script>
+
+<template>
+  <p>
+    currentSlideNo from useNav(): {{ currentSlideNo }} |
+  </p>
+</template>
---
+++ global-bottom.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer 
+    class="absolute bottom-0 left-0 w-full bg-white z-2"
+  >
+    <SlideNo />
+    <p>
+      $nav.currentSlideNo : {{ $nav.currentSlideNo  }}
+    </p>
+  </footer>
+</template>
